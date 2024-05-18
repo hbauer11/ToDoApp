4c8f3bf35bae8878810fab196aabbf6ec5c62ff4
hbauer11 <166079554+hbauer11@users.noreply.github.com>
2024-05-18 16:04:53 -0700

    Update unittests.test.js

--- unittests.test.js
+++ unittests.test.js
@@ -6,21 +6,21 @@ describe('ToDo List Tests', () => {
     tasks.length = 0;
   });
 
-  test('Add a task', () => {
+  testAddTask = ('Add a task', () => {
     addTask('New Task');
     expect(tasks).toHaveLength(1);
     expect(tasks[0].text).toBe('New Task');
     expect(tasks[0].completed).toBe(false);
   });
 
-  test('Delete a task', () => {
+  testDeleteTask = test('Delete a task', () => {
     addTask('Task to be deleted');
     const taskId = tasks[0].id;
     deleteTask(taskId);
     expect(tasks).toHaveLength(0);
   });
 
-  test('Toggle a task\'s completion', () => {
+  testToggleTask = ('Toggle a task\'s completion', () => {
     addTask('Task to be toggled');
     const taskId = tasks[0].id;
     toggleTask(taskId);
